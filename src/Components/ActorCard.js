b3ce4b0d23682103af3913a39292b73333db6b97
Delta de Dirac <igorshevtsov@gmail.com>
2021-04-12 11:39:56 +0300

    dark theme

--- src/Components/ActorCard.js
+++ src/Components/ActorCard.js
@@ -25,28 +25,39 @@ import Anthony from "./img/actors/AnthonyMackie.jpg";
 import Wallis from "./img/actors/WallisDay.jpg";
 
 export default function ActorCard() {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
   return (
     <Container>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" onClick={() => alert()}>
+          Actors Gallery
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl placeholder="Search Pattern" aria-label="Username" aria-describedby="basic-addon1" />
+            </InputGroup>
+          </Nav>
+          <Nav className="mr-auto" activeKey="1" onSelect={handleSelect}>
+            <NavDropdown title="Sort By" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1" eventKey="1">
+                First Name
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" eventKey="2">
+                Second Name
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" eventKey="3">
+                Age
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4" eventKey="4">
+                Descending Order
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
