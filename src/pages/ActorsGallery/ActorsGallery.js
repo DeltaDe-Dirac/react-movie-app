7fe2bc743c95c9ff3c595c9b449fb4419c931d5f
Delta de Dirac <igorshevtsov@gmail.com>
2021-04-21 21:53:26 +0300

    refactor useEffect using axios data promise

--- src/pages/ActorsGallery/ActorsGallery.js
+++ src/pages/ActorsGallery/ActorsGallery.js
@@ -10,74 +10,18 @@ import GalleryNavbar from "../../Components/GalleryNavBar/GalleryNavBar";
 import ActorCard from "../../Components/ActorCard/ActorCard";
 import ActorClass from "../../model/ActorClass";
 
-export default function ActorsGallery({ actors, pathPre, rowSize }) {
+export default function ActorsGallery({ dataPath, pathPre, rowSize }) {
   const [searchPattern, setSearchPatter] = useState("");
   const [sortFields, setSortFields] = useState(Array(4).fill(false));
   const [actorsData2Display, setActorsData2Display] = useState(null);
 
   useEffect(() => {
-    const regexp =
-      searchPattern.trim().length < 2
-        ? new RegExp(/.*/, "i")
-        : new RegExp(searchPattern.trim().replace(/ /g, "|"), "i");
-
-    axios
-      .get(actors)
-      .then((response) => {
-        const actorsDataFiltered = response.data
-          .map((actor) => new ActorClass(actor.fname, actor.lname, actor.born, actor.imgsrc))
-          .filter((actor) => regexp.test(actor.fname) || regexp.test(actor.lname) || regexp.test(actor.age))
-          .sort((actor1, actor2) => sortAny(actor1, actor2))
-          .map((actor) => (
-            <Col key={uuidv4()}>
-              <ActorCard
-                key={uuidv4()}
-                fname={actor.fname}
-                lname={actor.lname}
-                imgsrc={pathPre.concat(actor.imgsrc)}
-                born={actor.born}
-                age={actor.age}
-              />
-            </Col>
-          ));
-
-        const actors = _.chunk(actorsDataFiltered, rowSize).map((columns) => <Row key={uuidv4()}>{columns}</Row>);
-        setActorsData2Display(actors);
+    handleData(searchPattern, sortFields, dataPath, pathPre, rowSize)
+      .then((data) => {
+        setActorsData2Display(data);
       })
-      .catch((error) => {
-        console.error(error);
-      });
-
-    function sortAny(actor1, actor2) {
-      let [isSortByFName, isSortByLName, isSortByAge, sortOrder] = sortFields;
-      sortOrder = sortOrder ? -1 : 1;
-      let sortResult = 0;
-
-      if (isSortByFName) {
-        sortResult = sortStrings(actor1.fname, actor2.fname);
-      }
-
-      if (isSortByLName && sortResult === 0) {
-        sortResult = sortStrings(actor1.lname, actor2.lname);
-      }
-
-      if (isSortByAge && sortResult === 0) {
-        sortResult = actor1.age - actor2.age;
-      }
-      return sortOrder * sortResult;
-    }
-
-    function sortStrings(str1, str2) {
-      if (str1 < str2) {
-        return -1;
-      }
-      if (str1 > str2) {
-        return 1;
-      }
-
-      return 0;
-    }
-  }, [searchPattern, sortFields, actors, pathPre, rowSize]);
+      .catch((err) => console.log(err));
+  }, [searchPattern, sortFields, dataPath, pathPre, rowSize]);
 
   return (
     <Container>
@@ -85,4 +29,62 @@ export default function ActorsGallery({ actors, pathPre, rowSize }) {
       <div data-testid="actorsGrid">{actorsData2Display}</div>
     </Container>
   );
+}
+
+function handleData(searchPattern, sortFields, dataPath, pathPre, rowSize) {
+  const regexp =
+    searchPattern.trim().length < 2 ? new RegExp(/.*/, "i") : new RegExp(searchPattern.trim().replace(/ /g, "|"), "i");
+
+  const promise = axios.get(dataPath);
+  const dataPromise = promise.then((response) => {
+    const actorsDataFiltered = response.data
+      .map((actor) => new ActorClass(actor.fname, actor.lname, actor.born, actor.imgsrc))
+      .filter((actor) => regexp.test(actor.fname) || regexp.test(actor.lname) || regexp.test(actor.age))
+      .sort((actor1, actor2) => sortAny(actor1, actor2, sortFields))
+      .map((actor) => (
+        <Col key={uuidv4()}>
+          <ActorCard
+            key={uuidv4()}
+            fname={actor.fname}
+            lname={actor.lname}
+            imgsrc={pathPre.concat(actor.imgsrc)}
+            born={actor.born}
+            age={actor.age}
+          />
+        </Col>
+      ));
+    const actors = _.chunk(actorsDataFiltered, rowSize).map((columns) => <Row key={uuidv4()}>{columns}</Row>);
+    return actors;
+  });
+  return dataPromise;
+}
+
+function sortAny(actor1, actor2, sortFields) {
+  let [isSortByFName, isSortByLName, isSortByAge, sortOrder] = sortFields;
+  sortOrder = sortOrder ? -1 : 1;
+  let sortResult = 0;
+
+  if (isSortByFName) {
+    sortResult = sortStrings(actor1.fname, actor2.fname);
+  }
+
+  if (isSortByLName && sortResult === 0) {
+    sortResult = sortStrings(actor1.lname, actor2.lname);
+  }
+
+  if (isSortByAge && sortResult === 0) {
+    sortResult = actor1.age - actor2.age;
+  }
+  return sortOrder * sortResult;
+}
+
+function sortStrings(str1, str2) {
+  if (str1 < str2) {
+    return -1;
+  }
+  if (str1 > str2) {
+    return 1;
+  }
+
+  return 0;
 }
